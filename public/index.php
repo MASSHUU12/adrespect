df73d8e577da7a8821ece7563e2487befc8d39cb
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-06-15 15:13:49 +0200

    Removed unneeded comments

--- public/index.php
+++ public/index.php
@@ -7,15 +7,3 @@ use Dotenv\Dotenv;
 Dotenv::createImmutable("../")->load();
 
 require_once "./views/index.php";
-
-//DB::connect();
-
-//$query = DB::query("SELECT value FROM test");
-
-//DB::disconnect();
-
-//print_r($query->fetch_all());
-
-//Log::error("Hi");
-
-//NBP::get_exchange_rates();
