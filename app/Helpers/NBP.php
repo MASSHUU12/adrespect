9f68de0f8809d34804bd93a2d31aa5a0feb146f2
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-06-15 15:10:57 +0200

    Saving exchange rates to the database

--- app/Helpers/NBP.php
+++ app/Helpers/NBP.php
@@ -8,6 +8,11 @@ class NBP
 {
     private static string $url = 'http://api.nbp.pl/api/';
 
+    /**
+     * Fetches the latest exchange rates from the NBP API.
+     *
+     * @return array|bool An array containing exchange rate data or false if there was an error.
+     */
     public static function get_exchange_rates(): array|bool
     {
         try {
@@ -16,5 +21,43 @@ class NBP
             Log::error($exception);
             return false;
         }
+    }
+
+    /**
+     * Saves the exchange rates to the database.
+     *
+     * @param array $response The response array containing exchange rate data.
+     * @return bool True if the exchange rates were successfully saved, false otherwise.
+     */
+    public static function save_exchange_rates(array $response): bool
+    {
+        $table = $response[0]['table'];
+        $no = $response[0]['no'];
+        $effective_date = $response[0]['effectiveDate'];
+
+        $rates = $response[0]['rates'];
+
+        foreach ($rates as $rate) {
+            $currency = $rate['currency'];
+            $code = $rate['code'];
+            $mid = $rate['mid'];
+
+            try {
+                DB::query("
+                INSERT INTO `exchange_rates` (
+                    `id`, `table_name`, `number`, `effective_date`, `currency`, `code`, `mid`
+                ) VALUES (
+                    NULL, '$table', '$no', '$effective_date', '$currency', '$code', '$mid'
+                )");
+            } catch (Exception $exception) {
+                Log::error($exception);
+                return false;
+            }
+        }
+        DB::disconnect();
+
+        Log::error('Exchange rates successfully uploaded to the database.', false);
+
+        return true;
     }
 }
--- public/views/index.php
+++ public/views/index.php
@@ -19,12 +19,13 @@ use App\Helpers\NBP;
 if (isset($_POST['delete-db']))
     echo 'delete';
 if (isset($_POST['save-exchange-rates']))
-    print_r(NBP::get_exchange_rates());
+    NBP::save_exchange_rates(NBP::get_exchange_rates());
+
 ?>
 <main>
     <form action="#" method="post">
         <input type="submit" name="delete-db" value="Delete db"/>
-        <input type="submit" name="get-exchange-rates" value="Save exchange rates"/>
+        <input type="submit" name="save-exchange-rates" value="Save exchange rates"/>
     </form>
 </main>
 </body>
