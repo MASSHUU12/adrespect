da5d86bcda54eb233c077d579760e4b64fc6f743
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-06-17 21:24:28 +0200

    Updated and expanded the generic Model class

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -3,8 +3,7 @@
 namespace App\Models;
 
 use App\Helpers\DB;
-use App\Helpers\Log;
-use Exception;
+use PDOStatement;
 
 class Model
 {
@@ -26,10 +25,9 @@ class Model
      * Create a new record in the database.
      *
      * @param array $values The attribute values to be inserted.
-     * @return bool True if the record was successfully created, false otherwise.
-     * @throws Exception If there is an error executing the database query.
+     * @return PDOStatement|bool Returns PDOStatement on success or false on failure.
      */
-    public static function create(array $values): bool
+    public static function create(array $values): PDOStatement|bool
     {
         $query = sprintf(
             "INSERT INTO %s (%s) VALUES (%s)",
@@ -38,19 +36,13 @@ class Model
             self::create_placeholders($values)
         );
 
-        try {
-            DB::query($query, array_values($values));
-        } catch (Exception $exception) {
-            Log::error($exception);
-            return false;
-        }
-        return true;
+        return DB::query($query, array_values($values));
     }
 
     /**
      * Get the column names as a comma-separated string.
      *
-     * @return string The column names.
+     * @return string Returns the column names.
      */
     protected static function get_columns(): string
     {
@@ -62,12 +54,45 @@ class Model
      * Create placeholders for the given values as a comma-separated string.
      *
      * @param array $values The attribute values.
-     * @return string The placeholders.
+     * @return string Returns the placeholders.
      */
     protected static function create_placeholders(array $values): string
     {
         // Using late static binding to get access to the override variables
         return implode(',', array_fill(0, count($values), '?'));
+    }
+
+    /**
+     * Execute a database query with optional parameters.
+     *
+     * @param string $sql The SQL query string.
+     * @param array $params [optional] The parameters to bind to the query.
+     * @return PDOStatement|bool Returns PDOStatement on success or false on failure.
+     */
+    public static function query(string $sql, array $params = []): PDOStatement|bool
+    {
+        $statement = DB::query($sql, $params);
+
+        if (!$statement) {
+            return false;
+        }
+        return $statement;
+    }
+
+    /**
+     * Retrieve all records from the associated table.
+     *
+     * @return PDOStatement|bool Returns PDOStatement on success or false on failure.
+     */
+    public static function get(): PDOStatement|bool
+    {
+        $query = sprintf("SELECT * FROM %s", static::$table);
+        $statement = DB::query($query);
+
+        if (!$statement) {
+            return false;
+        }
+        return $statement;
     }
 
     /**
