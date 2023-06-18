1b23f7bd5ab791d726471bad52706c07a3b5b002
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-06-18 14:01:32 +0200

    Command for validating environment variables

--- app/Commands/EnvValidateCommand.php
+++ app/Commands/EnvValidateCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Commands;
 
+use App\Helpers\EnvValidator;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -12,7 +13,15 @@ class EnvValidateCommand extends Command
 {
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $output->writeln('Env');
+        $errors = EnvValidator::validate();
+
+        foreach ($errors as $err) {
+            $output->writeln($err);
+        }
+
+        if (count($errors) < 1) {
+            $output->writeln("Everything looks good.");
+        }
 
         return Command::SUCCESS;
     }
