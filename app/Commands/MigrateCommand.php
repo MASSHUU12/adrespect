73f45ef234f3ad73746e4d5c3a500f5d6ec3366c
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-06-18 16:21:26 +0200

    Added support for currency_conversions migration

--- app/Commands/MigrateCommand.php
+++ app/Commands/MigrateCommand.php
@@ -3,6 +3,7 @@
 namespace App\Commands;
 
 use App\Helpers\DB;
+use App\Models\CurrencyConversionsModel;
 use App\Models\ExchangeRatesModel;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
@@ -19,29 +20,27 @@ class MigrateCommand extends Command
     {
         DB::connect();
 
-        $truncate = ExchangeRatesModel::truncate();
-
-        if ($truncate !== false) {
+        // Exchange rates migration
+        if (ExchangeRatesModel::truncate() !== false) {
             $output->writeln('Truncating exchange_rates table.');
         } else {
-            $create = ExchangeRatesModel::create();
-
-            if ($create === false) {
-                $output->writeln(
-                    'There was a problem while creating the table. Make sure the database is running and the connection to it is configured correctly'
-                );
-
-                DB::disconnect();
-
-                return Command::FAILURE;
+            if (ExchangeRatesModel::create() !== false) {
+                $output->writeln('Created exchange_rates table.');
             }
-
-            $output->writeln('Created exchange_rates table.');
         }
 
-        DB::disconnect();
+        // Currency conversions migration
+        if (CurrencyConversionsModel::truncate() !== false) {
+            $output->writeln('Truncating exchange_rates table.');
+        } else {
+            if (CurrencyConversionsModel::create() !== false) {
+                $output->writeln('Created exchange_rates table.');
+            }
+        }
 
         $output->writeln('Migration completed.');
+
+        DB::disconnect();
 
         return Command::SUCCESS;
     }
