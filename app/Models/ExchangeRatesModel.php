5dce1a43814ff2e96363d67c99a2996925754377
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-06-18 15:32:53 +0200

    ExchangeRatesModel.php now support table creating

--- app/Models/ExchangeRatesModel.php
+++ app/Models/ExchangeRatesModel.php
@@ -2,10 +2,28 @@
 
 namespace App\Models;
 
+use App\Helpers\DB;
+use PDOStatement;
+
 class ExchangeRatesModel extends Model
 {
     protected static string $table = 'exchange_rates';
     protected static array $fillable = [
         'table_name', 'number', 'effective_date', 'currency', 'code', ' mid'
     ];
+
+    public static function create(): PDOStatement|bool
+    {
+        $query = sprintf('CREATE TABLE %s (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            table_name VARCHAR(50),
+            number VARCHAR(50),
+            effective_date DATE,
+            currency VARCHAR(100),
+            code VARCHAR(10),
+            mid DECIMAL(10, 4)
+        )', static::$table);
+
+        return DB::query($query);
+    }
 }
