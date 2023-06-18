70c76a21a01162aeda045444ae0160afaba6cdfb
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-06-18 14:55:35 +0200

    Improvements in MigrateCommand.php

--- app/Commands/MigrateCommand.php
+++ app/Commands/MigrateCommand.php
@@ -3,7 +3,7 @@
 namespace App\Commands;
 
 use App\Helpers\DB;
-use Exception;
+use App\Models\ExchangeRatesModel;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -19,14 +19,12 @@ class MigrateCommand extends Command
     {
         DB::connect();
 
-        try {
-            DB::query('TRUNCATE TABLE exchange_rates');
-            $output->writeln('Truncating exchange_rates table.');
-        } catch (Exception) {
-            $output->writeln('There is no exchange_rates table. Creating it now.');
+        $error_truncating = ExchangeRatesModel::truncate();
 
-            try {
-                DB::query('
+        if ($error_truncating !== false) {
+            $output->writeln('Truncating exchange_rates table.');
+        } else {
+            $statement = DB::query('
                     CREATE TABLE exchange_rates (
                         id INT AUTO_INCREMENT PRIMARY KEY,
                         table_name VARCHAR(50),
@@ -37,9 +35,14 @@ class MigrateCommand extends Command
                         mid DECIMAL(10, 4)
                     )
                 ');
-            } catch (Exception) {
-                $output->writeln('There was a problem while creating the table.
-                    Make sure the database is running and the connection to it is configured correctly');
+
+            if ($statement === false) {
+                $output->writeln(
+                    'There was a problem while creating the table. Make sure the database is running and the connection to it is configured correctly'
+                );
+
+                DB::disconnect();
+
                 return Command::FAILURE;
             }
         }
