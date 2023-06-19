c2271ab6cabb3cfceaacb92cfc628101916a38c1
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-06-19 12:01:58 +0200

    Added comments & improved validation

--- public/views/index.php
+++ public/views/index.php
@@ -21,11 +21,13 @@ use App\Models\ExchangeRatesModel;
 
 <body>
 <main>
+    <?php # Form with database actions?>
     <form action="#" method="post">
         <input type="submit" name="save-exchange-rates" value="Save exchange rates"/>
         <input type="submit" name="generate-exchange-rates-table" value="Generate exchange rates table"/>
         <input type="submit" name="generate-currency-conversions-list" value="Generate currency conversions table"/>
     </form>
+    <?php # Form for currency conversion?>
     <form action="#" method="post">
         <label for="currency-amount">Amount:</label>
         <input type="number" value="0" name="currency-amount" min="0" required/>
@@ -37,14 +39,23 @@ use App\Models\ExchangeRatesModel;
     </form>
 </main>
 <?php
+/**
+ * Get currency exchange rates and save in the database
+ */
 if (isset($_POST['save-exchange-rates'])) {
     NBP::save_exchange_rates(NBP::get_exchange_rates());
 }
+
+/**
+ * Download currency exchange rates from the database and create HTML table
+ */
 if (isset($_POST['generate-exchange-rates-table'])) {
     echo NBP::generate_exchange_rates_table();
 }
 
-// Converter form
+/**
+ * Convert from one currency to another
+ */
 if (isset($_POST['currency-convert'])) {
     $amount = $_POST['currency-amount'] ?? null;
     $source = $_POST['currency-source'] ?? null;
@@ -52,16 +63,21 @@ if (isset($_POST['currency-convert'])) {
 
     $amount_as_int = Convert::str_to_int($amount);
 
-    if (!is_int($amount_as_int) || is_null($source) || is_null($target)) {
+    // Check if the data received is valid
+    if (!is_int($amount_as_int) ||
+        !Helpers::is_currency_code_valid($source) ||
+        !Helpers::is_currency_code_valid($target)) {
         echo 'Incorrect data received';
         return;
     }
 
+    // Connect to the database
     if (!DB::connect()) {
         echo 'Connection with database failed.';
         return;
     }
 
+    // Prepare the SQL query
     $sql = 'SELECT code, mid FROM exchange_rates WHERE code IN (
         :source, :target
     ) ORDER BY FIELD(
@@ -69,6 +85,7 @@ if (isset($_POST['currency-convert'])) {
     )';
     $params = [':source' => $source, ':target' => $target];
 
+    // Execute the query and fetch the result
     $result = ExchangeRatesModel::query($sql, $params)->fetchAll();
     ExchangeRatesModel::disconnect();
 
@@ -82,16 +99,23 @@ if (isset($_POST['currency-convert'])) {
     $db_source_name = $result[0][0];
     $db_target_mid = $result[1][1];
     $db_target_name = $result[1][0];
+
+    // Convert the currency
     $converted = Convert::currency($amount_as_int, $db_source_mid, $db_target_mid);
 
+    // Insert the conversion into the database
     CurrencyConversionsModel::insert([$source, $amount, $target, $converted]);
 
     echo "Source: $amount $db_source_name, converted: $converted $db_target_name";
 }
 
+/**
+ * Download latest currency conversions from the database and create ordered list
+ */
 if (isset($_POST['generate-currency-conversions-list'])) {
     $out = [];
 
+    // Retrieve the currency conversions from the database
     foreach (CurrencyConversionsModel::get(10, ['conversion_date'], false)->fetchAll(PDO::FETCH_ASSOC) as $elem) {
         $source_amount = $elem['source_currency_amount'];
         $source_code = $elem['source_currency_code'];
@@ -99,9 +123,11 @@ if (isset($_POST['generate-currency-conversions-list'])) {
         $target_code = $elem['target_currency_code'];
         $conversion_date = $elem['conversion_date'];
 
+        // Format the conversion information
         $out[] = "$conversion_date | $source_amount $source_code => $target_amount $target_code";
     }
 
+    // Generate an ordered list
     echo Helpers::generate_list($out);
 }
 ?>
